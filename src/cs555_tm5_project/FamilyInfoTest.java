da728aa9e41aee548c76ef676431f71a66eda92a
tony wang <tonywinatusa@gmail.com>
2015-03-01 18:03:29 -0500

    New structure
    All the member function should be in the MemberFunction package. The
    Helper package includes all kinds of basic functions that can be used
    by all of us.

---
+++ src/cs555_tm5_project/FamilyInfoTest.java
@@ -0,0 +1,19 @@
+package cs555_tm5_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FamilyInfoTest {
+
+	@Test
+	public void testGetHusband() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHusband() {
+		fail("Not yet implemented");
+	}
+
+}

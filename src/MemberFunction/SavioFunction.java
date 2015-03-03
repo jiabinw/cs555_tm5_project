e4e247cd03f35926a3a51052f3fac22234ab2b0a
saviodrz <saviodrz@gmail.com>
2015-03-02 23:34:43 -0500

    Changes to SavioFunction.java
    Made changes to methods for US01 & US02

--- src/MemberFunction/SavioFunction.java
+++ src/MemberFunction/SavioFunction.java
@@ -1,14 +1,18 @@
 package MemberFunction;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 
+import Helper.Global;
 import cs555_tm5_project.FamilyInfo;
 import cs555_tm5_project.IndividualInfo;
 
 public class SavioFunction {
 	public static void deathBeforeBirth(HashMap individualInfo) {
+		System.out.println("***** Output for US01 *****\n");
+		
 		if(individualInfo != null && !individualInfo.isEmpty()) {
 			Object[] indiKey = individualInfo.keySet().toArray();
             Arrays.sort(indiKey);
@@ -18,14 +22,23 @@ public class SavioFunction {
                 
             	if(indiInfo.getBirthDate()!=null && indiInfo.getDeathDate()!=null) {
             		if(indiInfo.getDeathDate().before(indiInfo.getBirthDate())) {
-            			System.out.println(indiInfo.getName() + " has an incorrect Birth or Death Date.");
+            			String id = Global.rebuildIdentifier(retval.toString(), 'I');
+            			String name = indiInfo.getName();
+            			String bDay = new SimpleDateFormat("dd MMM yyyy").format(indiInfo.getBirthDate());
+            			String dDay = new SimpleDateFormat("dd MMM yyyy").format(indiInfo.getDeathDate());
+            					
+            			System.out.println("Individual " + id + " (" + name + ") has death date (" + dDay + ") before birth date (" + bDay + ")");
             		}
             	}
             }
 		}
+		
+		System.out.println("\n***** End of Output for US01 *****\n");
 	}
 	
-	public static void deathBeforeMarriage(HashMap individualInfo, HashMap familyInfo) {
+	public static void marriageBeforeBirth(HashMap individualInfo, HashMap familyInfo) {
+		System.out.println("***** Output for US02 *****\n");
+		
 		if(familyInfo != null && !familyInfo.isEmpty()) {
 			Object[] famKey = familyInfo.keySet().toArray();
 	        Arrays.sort(famKey);
@@ -35,19 +48,34 @@ public class SavioFunction {
             	
             	if(famInfo.getMarriageDate()!=null) {
             		Date marriageDate = famInfo.getMarriageDate();
+            		String mrDay = new SimpleDateFormat("dd MMM yyyy").format(marriageDate);
             		
             		if(famInfo.getHusband()!=0 && famInfo.getWife()!=0) {
             			IndividualInfo husbandInfo = (IndividualInfo)individualInfo.get(famInfo.getHusband());
             			IndividualInfo wifeInfo = (IndividualInfo)individualInfo.get(famInfo.getWife());
             			
-            			if(husbandInfo!=null && husbandInfo.getDeathDate()!=null && husbandInfo.getDeathDate().before(marriageDate))
-            				System.out.println(husbandInfo.getName() + " has an incorrect Marriage or Death Date.");
+            			String id, name, bDay;
             			
-            			if(wifeInfo!=null && wifeInfo.getDeathDate()!=null && wifeInfo.getDeathDate().before(marriageDate))
-            				System.out.println(wifeInfo.getName() + " has an incorrect Marriage or Death Date.");
+            			if(husbandInfo!=null && husbandInfo.getBirthDate()!=null && marriageDate.before(husbandInfo.getBirthDate())) {
+            				id = Global.rebuildIdentifier(Integer.toString(famInfo.getHusband()), 'I');
+                			name = husbandInfo.getName();
+                			bDay = new SimpleDateFormat("dd MMM yyyy").format(husbandInfo.getBirthDate());
+                			
+                			System.out.println("Individual " + id + " (" + name + ") has marriage date (" + mrDay + ") before birth date (" + bDay + ")");
+            			}
+            			
+            			if(wifeInfo!=null && wifeInfo.getBirthDate()!=null && marriageDate.before(wifeInfo.getBirthDate())) {
+            				id = Global.rebuildIdentifier(Integer.toString(famInfo.getWife()), 'I');
+                			name = wifeInfo.getName();
+                			bDay = new SimpleDateFormat("dd MMM yyyy").format(wifeInfo.getBirthDate());
+                			
+                			System.out.println("Individual " + id + " (" + name + ") has marriage date (" + mrDay + ") before birth date (" + bDay + ")");
+            			}
             		}
             	}
             }
 		}
+		
+		System.out.println("\n***** End of Output for US02 *****\n");
 	}
 }

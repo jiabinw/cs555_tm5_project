a9e75b3313b297ad8acf0d8dedb77bb95d039099
zhengsy741 <zhengsy741@gmail.com>
2015-03-08 20:01:22 -0400

    us15 and us18 update

--- src/MemberFunction/SiyuanFunction.java
+++ src/MemberFunction/SiyuanFunction.java
@@ -114,4 +114,102 @@ public class SiyuanFunction {
 		
 		
 	}
+<<<<<<< HEAD
+	
+	public static void childrenAmountExceed(HashMap individualInfo, HashMap familyInfo, int limit)
+	{
+		System.out.println("-----------------------------");
+		System.out.println("Output for US15: List the family has more than limit amount of children ");
+		System.out.println("-----------------------------\n");
+		String result="";
 
+		
+		if (familyInfo != null && !familyInfo.isEmpty()) {
+			Object[] famKey = familyInfo.keySet().toArray();
+			Arrays.sort(famKey);
+			int famID=0;
+			for(Object fam:famKey)
+			{
+				famID++;
+				FamilyInfo famInfo = (FamilyInfo) familyInfo.get(fam);
+			
+				if(famInfo.getChildren()!=null){
+					ArrayList<Integer> famChildren = famInfo.getChildren();
+					
+					
+					if(famChildren.size()>limit){
+						if(famInfo.getHusband()!=0 && famInfo.getWife()!=0){
+							IndividualInfo husband = (IndividualInfo) individualInfo.get(famInfo.getHusband());
+							IndividualInfo wife = (IndividualInfo) individualInfo.get(famInfo.getWife());
+							
+							String theFamID = Global.rebuildIdentifier(Integer.toString(famID) , 'F');
+							result = "Family "+theFamID
+									+" with husband @I"+famInfo.getHusband()+" ("+ husband.getName() +")"
+									+" and wife @I"+famInfo.getWife() +" ("+ wife.getName()+")"
+									+" has more than " + limit +" children.";
+						}
+					}
+				}
+			}
+		}
+		System.out.println(result);
+
+		
+		System.out.println("-----------------------------");
+		System.out.println("End of Output for US15 ");
+		System.out.println("-----------------------------\n");
+
+	}
+	
+	
+	public static void genderRatio(HashMap individualInfo, HashMap familyInfo){
+		System.out.println("-----------------------------");
+		System.out.println("Output for US18: List the gender ratio for each family ");
+		System.out.println("-----------------------------\n");
+		String result="";
+			
+		if (familyInfo != null && !familyInfo.isEmpty()) {
+			Object[] famKey = familyInfo.keySet().toArray();
+			Arrays.sort(famKey);
+			int famID=0;
+			for(Object fam:famKey)
+			{
+				famID++;
+				FamilyInfo famInfo = (FamilyInfo) familyInfo.get(fam);
+				
+				int maleThisFam=0;
+				int femaleThisFam=0;
+				
+				if(famInfo.getHusband()!=0)
+					maleThisFam++;
+				if(famInfo.getWife()!=0)
+					femaleThisFam++;
+				if(famInfo.getChildren()!=null){
+					ArrayList<Integer> children = famInfo.getChildren();
+					for(Integer child:children){
+						IndividualInfo childInfo = (IndividualInfo)individualInfo.get(child);
+						if(childInfo.getSex().equals("M"))
+							maleThisFam++;
+						if(childInfo.getSex().equals("F"))
+							femaleThisFam++;
+					}
+				}
+				result="Family @F"+famID+" has male members #"+maleThisFam
+						+" and female members #"+femaleThisFam;
+				
+				System.out.println(result);
+		}
+			}
+
+		
+		System.out.println("-----------------------------");
+		System.out.println("End of Output for US18 ");
+		System.out.println("-----------------------------\n");
+	}
+	
+	
+	
+}
+=======
+
+>>>>>>> origin/master
